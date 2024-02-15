53966a40757425fb2dd3fc24ca3b9b052210a231
Seymur Aliyev <seymur.aliyev.711@gmail.com>
2024-02-15 21:33:30 +0400

    dark mode toggle

--- site.js
+++ site.js
@@ -1,10 +1,12 @@
 const darkModeBtn = document.getElementById('dark-mode__btn');
-const bodyBg = document.querySelector('.login-register__bg');
+// const bodyBg = document.querySelector('.login-register__bg, .body-bg, .detail-movie__bg--main, .not-found__bg');
+const bodyBg = document.querySelector('.toggle-dark-mode');
 const modeIcon = document.getElementById('mode-icon');
 
 darkModeBtn.addEventListener('click', function () {
     console.log("click");
     bodyBg.classList.toggle('dark-mode');
+    bodyBg.classList.toggle('body-bg__dark-mode');
     if (bodyBg.classList.contains('dark-mode')) {
         modeIcon.textContent = '☀️';
     } else {
